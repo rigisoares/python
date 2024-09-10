d08abd3bf6784904217cd4e50ddc2c97033dcb2d
rigisoares <69166346+rigisoares@users.noreply.github.com>
2024-09-10 20:23:39 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,67 +1,86 @@
-import requests
-import datetime
+import sqlite3
+import os
 import pandas as pd
 from dotenv import load_dotenv
-import os
-import pickle
-import json
-from sklearn.linear_model import LinearRegression
-from assets.config import api_metadados, routes, cols_modelo, cols_pre_proc
 import assets.utils as utils
+from assets.utils import logger
+import datetime
 
 load_dotenv()
-origem = "EWR"
-destino = "IAH"
-#destino = routes[origem]
-
-def ingest_api(metadados, key, origem, destino):
-   '''
-   api_result = requests.get(
-      metadados["path"],
-      params = {
-        'access_key': key,
-        "dep_iata" : origem,
-        "arr_iata" : destino
-        "limit" : 5
-      })
-   api_response = api_result.json()
-   '''
-   with open('data/amostra.json', 'r') as file:
-      api_response = json.loads(file.read())
-
-   df = pd.json_normalize(api_response["data"])
-   return df
 
 def data_clean(df, metadados):
+    '''
+    Função principal para saneamento dos dados
+    INPUT: Pandas DataFrame, dicionário de metadados
+    OUTPUT: Pandas DataFrame, base tratada
+    '''
+    df["data_voo"] = pd.to_datetime(df[['year', 'month', 'day']]) 
+    df = utils.null_exclude(df, metadados["cols_chaves"])
+    df = utils.convert_data_type(df, metadados["tipos_originais"])
     df = utils.select_rename(df, metadados["cols_originais"], metadados["cols_renamed"])
-    df = utils.convert_data_type(df, metadados["tipos_map"])
     df = utils.string_std(df, metadados["std_str"])
+
+    df.loc[:,"datetime_partida"] = df.loc[:,"datetime_partida"].str.replace('.0', '')
+    df.loc[:,"datetime_chegada"] = df.loc[:,"datetime_chegada"].str.replace('.0', '')
+
+    for col in metadados["corrige_hr"]:
+        lst_col = df.loc[:,col].apply(lambda x: utils.corrige_hora(x))
+        df[f'{col}_formatted'] = pd.to_datetime(df.loc[:,'data_voo'].astype(str) + " " + lst_col)
+    
+    logger.info(f'Saneamento concluído; {datetime.datetime.now()}')
     return df
 
-def feat_eng(df, origem, destino, table, db):
-   data = df.copy()
-   data["tempo_voo_esperado"] = (data["datetime_chegada_formatted"] - \
-                                 data["datetime_partida_formatted"]
-                                 ) / pd.Timedelta(hours=1)
-   data["dia_semana"] = data["data_voo"].dt.day_of_week 
-   data["distancia"] = utils.recupera_dist(origem, destino, table, db)
-   data["horario"] = data.loc[:,"datetime_partida_formatted"]\
-                         .dt.hour.apply(lambda x: utils.classifica_hora(x))
-   return data[cols_pre_proc]
+def feat_eng(df):
+    '''
+    Função ???????????????????????????
+    INPUT: ???????????????????????????
+    OUTPUT: ???????????????????????????
+    '''
+    df_dw = df.copy()
+    df_dw["tempo_voo_esperado"] = (df_dw["datetime_chegada_formatted"] - df_dw["datetime_partida_formatted"]) / pd.Timedelta(hours=1)
+    df_dw["tempo_voo_hr"] = df_dw["tempo_voo"] /60
+    df_dw["atraso"] = df_dw["tempo_voo_hr"] - df_dw["tempo_voo_esperado"]
+    df_dw["dia_semana"] = df_dw["data_voo"].dt.day_of_week #0=segunda
+    #colocar log info
 
-def aplica_modelo(df, filename):
-   df_processed = utils.pre_process(df)
-   cols_missing = set(cols_modelo) - set(df_processed.columns)
-   for col in cols_missing: df_processed[col] = False
-   df_processed = df_processed[cols_modelo]
-   clf_reg = pickle.load(open(filename, 'rb'))
-   predict = clf_reg.predict(df_processed)
-   return predict
+    return df_dw
+
+def save_data_sqlite(df):
+    try:
+        conn = sqlite3.connect("data/NyflightsDB.db")
+        logger.info(f'Conexão com banco estabelecida ; {datetime.datetime.now()}')
+    except:
+        logger.error(f'Problema na conexão com banco; {datetime.datetime.now()}')
+    c = conn.cursor()
+    df.to_sql('nyflights', con=conn, if_exists='replace')
+    conn.commit()
+    logger.info(f'Dados salvos com sucesso; {datetime.datetime.now()}')
+    conn.close()
+
+def fetch_sqlite_data(table):
+    try:
+        conn = sqlite3.connect("data/NyflightsDB.db")
+        logger.info(f'Conexão com banco estabelecida ; {datetime.datetime.now()}')
+    except:
+        logger.error(f'Problema na conexão com banco; {datetime.datetime.now()}')
+    c = conn.cursor()
+    c.execute(f"SELECT * FROM {table} LIMIT 5")
+    print(c.fetchall())
+    conn.commit()
+    conn.close()
 
 
 if __name__ == "__main__":
-   df = ingest_api(api_metadados, os.getenv('ACCESS_KEY'), origem, destino)
-   df = data_clean(df, api_metadados)
-   df = feat_eng(df, origem, destino, "nyflights", "data/NyflightsDB.db")
-   pred = aplica_modelo(df, "assets/reg_model.sav")
-   print(pred)
+    logger.info(f'Inicio da execução ; {datetime.datetime.now()}')
+    metadados  = utils.read_metadado(os.getenv('META_PATH'))
+    df = pd.read_csv(os.getenv('DATA_PATH'),index_col=0)
+    df = data_clean(df, metadados)
+    print(df.head())
+    utils.null_check(df, metadados["null_tolerance"])
+    utils.keys_check(df, metadados["cols_chaves"])
+    print(df.columns)
+    df = feat_eng(df)
+    print(df.columns)
+    #save_data_sqlite(df)
+    fetch_sqlite_data(metadados["tabela"][0])
+    logger.info(f'Fim da execução ; {datetime.datetime.now()}')
